e6770050aeb8b18e7e6b9ab9ff2960723fb63187
Lelton Crazy <leltondev@gmail.com>
2024-11-02 18:33:47 -0300

    chore: resolving warn

--- src/main/java/org/wiki/system/resource/response/PanacheQueryUtils.kt
+++ src/main/java/org/wiki/system/resource/response/PanacheQueryUtils.kt
@@ -5,12 +5,12 @@ import io.quarkus.mongodb.panache.kotlin.PanacheQuery
 fun <T : Any, R : Any> PanacheQuery<T>.toPaginatedResponse(
     page: Int,
     pageSize: Int,
-    map: (T) -> R = { it as R }
+    map: (T) -> R
 ): PaginatedResponse<R> {
     val items = this.page(page, pageSize)
         .list()
-        .map(map);
-    val totalCount = this.count();
-    val totalPages = (totalCount / pageSize) + 1;
+        .map(map)
+    val totalCount = this.count()
+    val totalPages = (totalCount / pageSize) + 1
     return PaginatedResponse(items, totalCount, totalPages.toInt(), page)
 }
