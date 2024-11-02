984155a1f7cf6461bb1c6c520e4bb9e2b4a756b0
Lelton Crazy <leltondev@gmail.com>
2024-11-02 19:44:28 -0300

    build: upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "2.0.10"
     kotlin("plugin.allopen") version "2.0.10"
     kotlin("plugin.serialization") version "2.0.10"
-    id("io.quarkus")
+    id("io.quarkus") version "3.16.1"
 }
 
 repositories {
